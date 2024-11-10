a56744f567664e963d4100b68c1166d36235608b
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2024-11-10 15:52:49 +0000

    feat-app: implement basic frontal face detection

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -15,6 +15,9 @@
 #include <thread>
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
 
 #include "Log.hpp"
 #include "Application.hpp"
@@ -24,21 +27,42 @@ void Application::start()
     Log::create(); //create singleton instance of Log
     Log::log_info("Initialising Seat Occupancy Detection...", "Application");
     Log::log_info("Entering app loop..", "Application");
-
-    std::string image_path = "../../../../resource/test.png"; // <- running from Debug dir
-    cv::Mat img = cv::imread(image_path);
-    cv::imshow("Display window", img);
-    cv::waitKey(0);
 }
 
 void Application::loop()
 {
     //TODO main application logic goes here
-    int i = 0;
-    while (i++ < 10) {
-        //TODO app logic goes here
-        Log::log_info("Info...", "Application");
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    cv::VideoCapture video(0); // <- web cam for test purposes
+    cv::Mat image;
+
+    std::string path_to_haarcascade = "../../../../resource/haarcascade_frontalface_default.xml";
+
+    cv::CascadeClassifier face_cascade;
+    face_cascade.load(path_to_haarcascade);
+
+
+    bool run_loop = true;
+    if (face_cascade.empty()) {
+        Log::log_info("XML file not loaded", "Application");
+        run_loop = false;
+    }
+
+    while (run_loop) {
+        video.read(image);
+
+        std::vector<cv::Rect> faces;
+        face_cascade.detectMultiScale(image, faces, 1.3, 5);
+
+        for (int i = 0; i < faces.size(); i++) {
+            //drawing rectangle around faces found in the output window (color: red, thickness: 3)
+            cv::rectangle(image, faces[i].tl(), faces[i].br(), cv::Scalar(50, 50, 255), 3);
+
+            //display count of faces found in top left corner of output window - testing purposes
+            cv::putText(image, std::to_string(faces.size()), cv::Point(10, 40), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255, 255, 255), 1);
+        }
+
+        cv::imshow("Frame", image);
+        cv::waitKey(1);
     }
 }
 
